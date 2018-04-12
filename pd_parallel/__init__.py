9860787fc18a00628eb0600240ec07751f6744fe
quantpy <quantpy@gmail.com>
2018-04-12 10:16:36 +0800

    add version num

--- pd_parallel/__init__.py
+++ pd_parallel/__init__.py
@@ -8,5 +8,9 @@
 @time:    2018/4/11 12:08
 """
 
+
 from .apply_parallel import df_group_apply_parallel, df_apply_parallel
 from .tools import get_grouper, double_groupby_apply_parallel
+
+
+__version__ = '0.1'
